5423a5db54aab3db2f3ccf7899777d11d031f86c
Puneet0123 <sainipuneet471@gmail.com>
2025-05-10 16:14:40 +0530

    Initial commit

--- app.py
+++ app.py
@@ -2,7 +2,8 @@ import streamlit as st
 import joblib
 import pandas as pd
 import numpy as np
-import os
+import math
+from sklearn.preprocessing import StandardScaler
 
 # Load model and scaler\
 thresholds = joblib.load('threshold_precision.pkl')
@@ -10,31 +11,44 @@ model = joblib.load("best_model.pkl")
 
 # Streamlit UI
 st.title(" Loan Approval Prediction App")
-
+st.markdown("""This app predicts whether a loan application is likely to be **approved or not** based on applicant details.
+            It uses a trained Machine Learnings model (e.g., Logistic Regression) on realworld loan data.""")
 st.markdown("Fill the form below to predict if the loan will be approved:")
 
 #Set minimun loan amount
 loan_amount = 1000
 
-# Input fields
-gender = st.selectbox("Gender", ['Male', 'Female'])
-married = st.selectbox("Married", ['Yes', 'No'])
-education = st.selectbox("Education", ['Graduate', 'Not Graduate'])
-self_employed = st.selectbox("Self Employed", ['Yes', 'No'])
-applicant_income = st.number_input("Applicant Income", min_value=0)
-coapplicant_income = st.number_input("Coapplicant Income", min_value=0)
-loan_amount = st.number_input("Loan Amount (in 1000s)", min_value=1000)
-loan_term = st.selectbox("Loan Term", [360, 180, 120, 84, 60])
-credit_history = st.selectbox("Credit History", [1.0, 0.0])
-property_area = st.selectbox("Property Area", ['Urban', 'Semiurban', 'Rural'])
-dependents = st.selectbox("Dependents", ['0', '1', '2', '3+'])
-
-
-total_income = applicant_income + coapplicant_income
+## Input Data
+with st.form("loan_form"):
+    col1, col2 = st.columns(2)
+    
+    with col1:
+        gender = st.selectbox("Gender",["Male","Female"])
+        married = st.selectbox("Married",["Yes","No"])
+        education = st.selectbox("Education", ["Graduate","Not Graduate"])
+        applicant_income = st.number_input("Applicant Income", min_value=1000)
+        coapplicant_income = st.number_input("Coapplicant Income", min_value = 0)
+        
+    with col2: 
+        dependents = st.selectbox("Dependents",["0","1","2","3+"])
+        self_employed = st.selectbox("Self Employed",["Yes","No"])
+        loan_amount = st.number_input("Loan Amount (in 1000s)", min_value=1000)
+        loan_term = st.number_input("Loan Term (in days)", min_value =60)
+        property_area = st.selectobx("Property Area", ["Urban","Semi Urban","Rural"])
+            
+    credit_history = st.selectbox("Credit History", ['1','0'])
+    
+    
+    submitted = st.form_submit_button("Predict Loan Status")      
+    
+# Preprocessing Input 
+total_income = applicant_income + coapplicant_income    
 income_loan_ratio = total_income / loan_amount
+log_loan_amount = math.log(loan_amount)
+scaler_arr =np.array([total_income,income_loan_ratio,log_loan_amount]).reshape(-1, 1)
+scaler = StandardScaler()
+data_arr = scaler.fit_transform(scaler_arr)
 
-
-# Preprocess input
 input_data = np.array([[
     1 if gender == 'Male' else 0,
     1 if married == 'Yes' else 0,
@@ -43,8 +57,9 @@ input_data = np.array([[
     loan_term,
     credit_history,
     total_income,
-    income_loan_ratio,
-    loan_amount,
+    data_arr[0],
+    data_arr[1],
+    data_arr[2],
     True if property_area =='Semiurban' else False,
     True if property_area == 'Urban' else False,
     True if dependents == 1 else False,
